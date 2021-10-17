4e4e04d445cbe0e95ba665a272827e084f894f52
Kaustubh Debnath <kaustubh.debnath10@gmail.com>
2021-10-17 17:04:31 +0530

    simplified bash script again

--- transcode.sh
+++ transcode.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh -i "EffluxKeys.pem" ubuntu@ec2-65-2-83-255.ap-south-1.compute.amazonaws.com
+
 
 echo "Efflux Video Transcoding"
 echo "Developed by Kaustubh Debnath and Sumit Prakash"
@@ -17,7 +17,7 @@ aws s3 cp s3://efflux-raw/$filepath
 ffmpeg -i $filepath -c:v copy -filter:v scale=720:-1 -c:a copy output-720.mp4
 aws s3 cp output-720.mp4 s3://efflux-raw/OUTPUT/output-720.mp4
 
-ffmpeg -i $filepath -c:v libx265  -filter:v scale=480:-1 -c:v copy output-720.mp4
+wget 
 
 
 # ffmpeg -i $filepath -c:v copy -c:a copy -tag:v hvc1 output.mp4
