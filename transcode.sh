e1e0d6d18f69aab796b94ad65acca1ad02db6971
Kaustubh Debnath <kaustubh.debnath10@gmail.com>
2021-11-18 16:28:59 +0530

    working script

--- transcode.sh
+++ transcode.sh
@@ -1,20 +1,23 @@
 #!/bin/bash -e
-
+  
 var7=$(ec2metadata --instance-id)
 
-aws autoscaling set-instance-protection --instance-ids $var7 --auto-scaling-group-name efflux-new-ASG --protected-from-scale-in
+# aws autoscaling set-instance-protection --instance-ids $var7 --auto-scaling-group-name efflux-new-ASG --protected-from-scale-in
+# export AWS_DEFAULT_REGION=ap-south-1
 
-main_obj=$(aws sqs receive-message --queue-url https://sqs.ap-south-1.amazonaws.com/265595266672/EffluxQueue --attribute-names All --message-attribute-names All --max-number-of-messages 1)
+main_obj=$(aws sqs receive-message --queue-url https://sqs.ap-south-1.amazonaws.com/265595266672/EffluxQueue)
 
 echo -e $main_obj > version.json
 
-var1=$(jq '.Messages[] | {Body} | .Body | fromjson | . | .Records[] | {s3} | .s3 | .bucket | .name'  version.json) 
+var1=$(jq '.Messages[] | {Body} | .Body | fromjson | . | .Records[] | {s3} | .s3 | .bucket | .name'  version.json)
 
 var2=$(jq '.Messages[] | {Body} | .Body | fromjson | . | .Records[] | {s3} | .s3 | .object | .key'  version.json)
 
 var3=$(echo s3://$var1/$var2 | tr -d '""')
 
 var4=$(echo $var2 | tr -d '""')
+
+echo "input file" $var4
 
 # var5=$(echo $var4-1080.mp4)
 
@@ -36,4 +39,4 @@ receipt_handle=$(echo $var8 | tr -d '""')
 
 aws sqs delete-message --queue-url https://sqs.ap-south-1.amazonaws.com/265595266672/EffluxQueue --receipt-handle $receipt_handle
 
-aws autoscaling set-instance-protection --instance-ids $var7 --auto-scaling-group-name efflux-new-ASG --no-protected-from-scale-in
+# aws autoscaling set-instance-protection --instance-ids $var7 --auto-scaling-group-name efflux-new-ASG --no-protected-from-scale-in
